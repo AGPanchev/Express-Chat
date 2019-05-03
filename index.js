b1d66b08151397b19a1f11631ba8e20fc48c4f4d
Antonio Panchev <antonio.panchev@newcryptoblock.io>
2019-05-03 15:10:33 +0300

    date

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var moment = require('moment');
+var now = moment().format('MMMM Do YYYY, h:mm:ss a')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,7 +11,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', now + ' - '+ msg);
   });
 });
 
